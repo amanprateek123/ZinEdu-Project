a70a7f27b9befcc599fa8c1eef4fc74274f5339b
Aman Prateek <amanpra333@gmail.com>
2020-05-31 17:24:05 +0530

    Updates

---
+++ precache-manifest.ee4378b2919c2e71e5ffcd097afe772f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d2dbc3686991b7bc62c6fe8b079533",
+    "url": "/ZinEdu-Project/index.html"
+  },
+  {
+    "revision": "9858370c3c2d752e5109",
+    "url": "/ZinEdu-Project/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d9e839f1072e94ddc381",
+    "url": "/ZinEdu-Project/static/css/main.96a7778b.chunk.css"
+  },
+  {
+    "revision": "9858370c3c2d752e5109",
+    "url": "/ZinEdu-Project/static/js/2.98470d60.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ZinEdu-Project/static/js/2.98470d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e839f1072e94ddc381",
+    "url": "/ZinEdu-Project/static/js/main.3615ec51.chunk.js"
+  },
+  {
+    "revision": "91bc6492a852745c24d2",
+    "url": "/ZinEdu-Project/static/js/runtime-main.23a8db42.js"
+  },
+  {
+    "revision": "c93023dd1f5b8fb2854390f5e499aa75",
+    "url": "/ZinEdu-Project/static/media/enroll.c93023dd.PNG"
+  },
+  {
+    "revision": "d9bf3d38a94adbb43882bbd2eca794c8",
+    "url": "/ZinEdu-Project/static/media/girl.d9bf3d38.jpg"
+  },
+  {
+    "revision": "b2663c194de93be9aaf88b4c7c47e59a",
+    "url": "/ZinEdu-Project/static/media/hands.b2663c19.jpg"
+  },
+  {
+    "revision": "b69f245529fd7dd0216651764963316b",
+    "url": "/ZinEdu-Project/static/media/logo.b69f2455.png"
+  },
+  {
+    "revision": "443478c3774fa561204ba9dbd32a6430",
+    "url": "/ZinEdu-Project/static/media/stats.443478c3.PNG"
+  },
+  {
+    "revision": "a0d94fce928521e0968052339e19cda7",
+    "url": "/ZinEdu-Project/static/media/thumb.a0d94fce.jpg"
+  }
+]);
